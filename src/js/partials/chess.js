f0b2adcee9d04ad59ccf295a86e78b06ac61efe0
niilka <daemon.named@gmail.com>
2018-04-20 01:22:21 +0300

    tests,tests,tests

--- src/js/partials/chess.js
+++ src/js/partials/chess.js
@@ -2,10 +2,10 @@
 
 class Table {
     createField() {
-        const tablePosition = document.getElementsByClassName('chess_table')[0];
+        const TABLE_POSITION = document.getElementsByClassName('chess_table')[0];
 
         let appendCell = function (cell) {
-            tablePosition.appendChild(cell);
+            TABLE_POSITION.appendChild(cell);
         };
 
         let createCell = function (cellName) {
@@ -14,66 +14,25 @@ class Table {
             cell.setAttribute('cell', cellName)
             appendCell(cell);
         }
-        let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-        let ranges = {
-            1: 1,
-            2: 1,
-            3: 1,
-            4: 1,
-            5: 1,
-            6: 1,
-            7: 1,
-            8: 1
-        }
+        const MAP = [
+            'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
+            'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
+            'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
+            'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
+            'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
+            'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
+            'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
+            'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'
+        ]
 
-        for (let i = 64; i >= 0; i--) {
-            if (i >= 56) {
-                if (ranges[8] < 9) {
-                    createCell(chars[ranges[8] - 1] + 8);
-                    ranges[8]++;
-                }
-            } else if (i >= 48) {
-                if (ranges[7] < 9) {
-                    createCell(chars[ranges[7] - 1] + 7);
-                    ranges[7]++;
-                }
-            } else if (i >= 40) {
-                if (ranges[6] < 9) {
-                    createCell(chars[ranges[6] - 1] + 6);
-                    ranges[6]++;
-                }
-            } else if (i >= 32) {
-                if (ranges[5] < 9) {
-                    createCell(chars[ranges[5] - 1] + 5);
-                    ranges[5]++;
-                }
-            } else if (i >= 24) {
-                if (ranges[4] < 9) {
-                    createCell(chars[ranges[4] - 1] + 4);
-                    ranges[4]++;
-                }
-            } else if (i >= 16) {
-                if (ranges[3] < 9) {
-                    createCell(chars[ranges[3] - 1] + 3);
-                    ranges[3]++;
-                }
-            } else if (i >= 8) {
-                if (ranges[2] < 9) {
-                    createCell(chars[ranges[2] - 1] + 2);
-                    ranges[2]++;
-                }
-            } else if (i >= 0) {
-                if (ranges[1] < 9) {
-                    createCell(chars[ranges[1] - 1] + 1);
-                    ranges[1]++;
-                }
-            }
-        }
+        MAP.forEach((cellName) => {
+            createCell(cellName);
+        }, this);
     }
 }
 
-class Figures extends Table {
+class Figures {
     getFigure(figure, color) {
         const FIGURES_COLLECTION = {
             king: {
@@ -106,23 +65,67 @@ class Figures extends Table {
         newFigure.setAttribute('team', color);
         newFigure.classList.add('figure');
         newFigure.innerHTML = FIGURES_COLLECTION[figure][color];
-
         return newFigure;
     }
 
     setFigure(cell, figure) {
         let allCells = document.getElementsByClassName('cell');
-
         [].forEach.call(allCells, checkedCell => {
             if (checkedCell.getAttribute('cell') === cell) {
                 checkedCell.appendChild(figure);
             }
         });
     }
+
+    setAllFigures() {
+        let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+        const FIGURES_TEMPLATE = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+        for (let i = 0; i < 8; i++) {
+            this.setFigure(`${chars[i]}1`, this.getFigure(FIGURES_TEMPLATE[i], 'white'));
+        }
+
+        for (let i = 0; i < 8; i++) {
+            this.setFigure(`${chars[i]}2`, this.getFigure('pawn', 'white'));
+        }
+
+        for (let i = 0; i < 8; i++) {
+            this.setFigure(`${chars[i]}8`, this.getFigure(FIGURES_TEMPLATE[i], 'black'));
+        }
+
+        for (let i = 0; i < 8; i++) {
+            this.setFigure(`${chars[i]}7`, this.getFigure('pawn', 'black'));
+        }
+    }
+
+    setSelectedEvent() {
+        let allFiguresOnDesk = document.getElementsByClassName('figure');
+        for (let i = 0; i < allFiguresOnDesk.length; i++) {
+            allFiguresOnDesk[i].addEventListener('click', function (event) {
+                for (let i = 0; i < allFiguresOnDesk.length; i++) {
+                    allFiguresOnDesk[i].classList.remove('selected');
+                }
+                event.target.classList.toggle('selected');
+            });
+        }
+    }
 }
 
 let table = new Table();
 table.createField();
 let figures = new Figures();
+figures.setAllFigures();
+figures.setSelectedEvent();
 
-figures.setFigure('d6', figures.getFigure('queen', 'white'));
+
+let allCells = document.getElementsByClassName('cell');
+[].forEach.call(allCells, checkedCell => {
+    checkedCell.addEventListener('click', (event) => {
+        let allFiguresOnDesk = document.getElementsByClassName('figure');
+        for (let i = 0; i < allFiguresOnDesk.length; i++) {
+            if (allFiguresOnDesk[i].classList.contains('selected')) {
+                event.target.appendChild(allFiguresOnDesk[i]);
+            }
+        }
+    });
+});
