46059a65a4094ce77d63dad0e72716474908bf2e
niilka <daemon.named@gmail.com>
2018-04-21 18:18:50 +0300

    Steps add steps (need rebuild)

--- src/js/partials/chess.js
+++ src/js/partials/chess.js
@@ -3,7 +3,6 @@
 // Game field
 
 class Field {
-
     static get fieldPosition() { // Get chess table element
         return document.querySelector('.chess_table');
     }
@@ -18,6 +17,65 @@ class Field {
 
     static get cells() { // Get all cells on chess board
         return document.querySelectorAll('.cell');
+    }
+
+    static get figures() {
+        return document.querySelectorAll('.figure');
+    }
+
+    cellEventHandler(event) {
+        let currentCell = event.target;
+        let self = this;
+
+        function moveFigure(figure, cell) {
+            cell.appendChild(figure);
+            self.cellHighlight(false);
+        }
+
+        function isHighlighted(cell) {
+            if (cell.classList.contains('highlight'))
+                return true;
+            return false;
+        }
+
+        function isSelected() {
+            if (document.getElementsByClassName('selected').length)
+                return true;
+            return false
+        }
+
+        function getSelectedFigure() {
+            let figure = document.getElementsByClassName('selected')[0];
+            return figure;
+        }
+
+        if (isHighlighted(currentCell)) {
+            if (isSelected())
+                moveFigure(getSelectedFigure(), currentCell);
+        }
+
+    }
+
+    addCellEvent() {
+        Field.cells.forEach((cell) => {
+            cell.addEventListener('mousedown', (event) => {
+                this.cellEventHandler(event)
+            });
+        });
+    }
+
+    cellHighlight(highlitedCells) {
+        if (highlitedCells) {
+            Field.cells.forEach((cell) => {
+                let currentCell = cell.getAttribute('cell');
+                if (highlitedCells.indexOf(currentCell) !== -1)
+                    cell.classList.add('highlight')
+            });
+        } else {
+            Field.cells.forEach((cell) => {
+                cell.classList.remove('highlight');
+            });
+        }
     }
 
     createField() { // Create field
@@ -72,6 +130,7 @@ class Figures extends Field {
         let newFigure = document.createElement('div');
         newFigure.classList.add('figure');
         newFigure.setAttribute('team', team);
+        newFigure.setAttribute('figure', figure);
         newFigure.innerHTML = Figures.figuresCollection[figure].symbol;
         return newFigure;
     }
@@ -127,9 +186,91 @@ class Figures extends Field {
             }
         }
     }
+
+    addFiguresEvent() {
+        Field.figures.forEach((figure) => {
+            figure.addEventListener('mousedown', (event) => {
+                this.figureEventHandler(event);
+            });
+        });
+    }
+
+    figureEventHandler(event) {
+        function changeSelected(selectFigure) {
+            Field.figures.forEach((figure) => {
+                if (figure.classList.contains('selected'))
+                    figure.classList.remove('selected');
+            })
+            selectFigure.classList.toggle('selected');
+        };
+        event.preventDefault();
+        event.stopPropagation();
+        changeSelected(event.target);
+        this.figureListener(event.target);
+    }
+
+    stepLogic(figureObject) {
+        switch (figureObject.figureName) {
+            case 'pawn':
+                super.cellHighlight(false);
+                super.cellHighlight(pawn(figureObject));
+                break;
+        }
+
+        function pawn(figureObject) {
+            let cellName = figureObject.cellName;
+            let team = figureObject.figureTeam;
+            let letter = cellName[0];
+            let number = parseInt(cellName[1]);
+            let steps = [];
+            if (team === 'white') {
+                steps = [letter + (number + 1), letter + (number + 2)];
+            } else {
+                steps = [letter + (number - 1), letter + (number - 2)];
+            }
+            return steps;
+        }
+    }
+
+    figureListener(figure) {
+        let figureObject = {
+            cellName: figure.parentNode.getAttribute('cell'),
+            figureName: figure.getAttribute('figure'),
+            figureTeam: figure.getAttribute('team')
+        }
+        this.stepLogic(figureObject);
+    }
+}
+
+class Figure extends Figures {
+    constructor(figure) {
+        this.steps;
+        this.eats;
+        this.cell = figure.cellName;
+        this.team = figure.teamName;
+        this.figureName = figure.figureName;
+    }
+
+    get steps() {
+        return this.steps;
+    }
+
+    set steps(value) {
+        this.steps = value;
+    }
+
+    get eats() {
+        return this.eats;
+    }
+
+    set eats(value) {
+        this.eats = value;
+    }
 }
 
 let field = new Field();
 field.createField();
+field.addCellEvent();
 let figures = new Figures();
 figures.buildTeams();
+figures.addFiguresEvent();
